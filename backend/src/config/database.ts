e6022294d000d1390ae34c9b4c2bbf25ef3336a2
Arthur Steiner <arthursteinerr>
2026-05-27 12:15:12 -0300

    Refatora conexão com o banco de dados para melhorar a estrutura e a entendimento do código

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,12 +1,17 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+pool
+  .connect()
+  .then(() => {
+    console.log("[DATABASE] PostgreSQL conectado com sucesso");
+  })
+  .catch((err) => {
+    console.error("[DATABASE] Erro ao conectar:", err);
+  });

e9390129838895ec15decccccba1094a8d5dec00
Joyce <joycemasallajorge2006@gmail.com>
2026-06-08 16:52:06 -0300

    limpa arquivo de teste offline

--- frontend/src/config/apiUrl.ts
+++ frontend/src/config/apiUrl.ts
@@ -1,5 +1,1 @@
 export const API_URL = "https://desenvolvimento-app-baby-guard.onrender.com";
- // Comente a linha do Render e use o seu IP local (da sua rede Wi-Fi) para testar o backend rodando no seu PC
- // export const API_URL = "https://desenvolvimento-app-baby-guard.onrender.com";
- 
- //export const API_URL = "http://192.168.0.999:3000";
